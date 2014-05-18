305fd0fb007de591cdded80e00323977582cb281
cdbajorin <christopher.bajorin@gmail.com>
2014-05-17 21:40:38 -0400

    env variable for api added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+url = "http://api.brewerydb.com/v2/search/geo/point?key=26f66fc43cc1993e96da0d91327578ee&lat=40.77&lng=-73.95&radius=100"
+
+breweries = HTTParty.get(url)
+
+
+
+breweries["data"].each do |b|
+  if b["openToPublic"] == "Y"
+    Brewery.create({
+      id: b["brewery"]["id"],
+      name: b["brewery"]["name"],
+      street_address: b["streetAddress"],
+      city: b["locality"],
+      state: b["region"],
+      zipcode: b["postalCode"],
+      latitude: b["latitude"],
+      longitude: b["longitude"],
+      distance: b["distance"]
+      })
+  end
+end
