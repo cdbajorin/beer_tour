56b850eb5e0f20050e1afe8fd103b44be5481cdd
cdbajorin <christopher.bajorin@gmail.com>
2014-05-19 12:06:51 -0400

    pre push

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,6 +1,7 @@
 class ToursController < ApplicationController
 
   def create
+    binding.pry
     @tour_array = params[:brewery_list]
     tour_string = @tour_array.join(",")
     @current_tour = Tour.new
