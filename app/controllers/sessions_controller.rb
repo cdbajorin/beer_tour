b81e26ee40ce58de5514614f2994e8ae363d991d
cdbajorin <christopher.bajorin@gmail.com>
2014-06-13 14:15:52 -0400

    routes set up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_username(params[:username])
     success = false
+
     if user != nil
       success = user.authenticate(params[:password])
     end
